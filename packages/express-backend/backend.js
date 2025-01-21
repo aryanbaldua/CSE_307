bca34f76350a37f6b877ec2dc5354e5e076ed70f
Aryan Baldua <67344835+aryanbaldua@users.noreply.github.com>
2025-01-21 15:54:16 -0800

    completed IE2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,21 +40,91 @@ const findUserByName = (name) => {
       (user) => user["name"] === name
     );
   };
+
+const findUserById = (id) => {
+return users["users_list"].find((user) => user["id"] === id);
+};
+const deleteUserById = (id) => {
+    const index = users["users_list"].findIndex((user) => user["id"] === id);
+    if (index !== -1) {
+      users["users_list"].splice(index, 1);
+      return true;
+    }
+    return false;
+};
+
+const findUsersByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name && user["job"] === job
+    );
+};
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
   
-  app.get("/", (req, res) => {
-    res.send("Welcome to the Express Backend!");
-  });
-  
-  app.get("/users", (req, res) => {
+app.get("/", (req, res) => {
+res.send("Welcome to the Express Backend!");
+});
+
+app.get("/users", (req, res) => {
+const name = req.query.name;
+if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.json(result);
+} else {
+    res.json(users);
+}
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.json(result);
+    }
+});
+
+
+app.get("/users", (req, res) => {
     const name = req.query.name;
-    if (name != undefined) {
+    const job = req.query.job;
+  
+    if (name !== undefined && job !== undefined) {
+      let result = findUsersByNameAndJob(name, job);
+      result = { users_list: result };
+      res.json(result);
+    } else if (name !== undefined) {
       let result = findUserByName(name);
       result = { users_list: result };
       res.json(result);
     } else {
       res.json(users);
     }
-  });
+});
+
+
+app.post("/users", (req, res) => {
+    console.log(req.body);
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    console.log("Updated users list:", users["users_list"]);
+    res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    const success = deleteUserById(id);
+    if (success) {
+      res.status(200).send("User deleted successfully.");
+    } else {
+      res.status(404).send("Resource not found.");
+    }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
